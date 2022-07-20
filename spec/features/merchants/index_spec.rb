f97724185118eff30fb65cbb84820e7106e481df
Wesley Garcia <wes@alltradestech.com>
2022-07-20 14:50:50 -0700

    Add tests for Merchants index and show views; run rubocop fixes

---
+++ spec/features/merchants/index_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Merchants Index View' do
+  describe 'User Story 1' do
+    # As a visitor,
+    # When I visit '/merchants'
+    # I should see a list of merchants by name
+    # and when I click the merchant's name
+    # I should be on page '/merchants/:id'
+
+    # And I should see a list of items that merchant sells.
+    it 'has links to each' do
+      visit '/merchants'
+
+      expect(page).to have_link('Schroeder-Jerde')
+      expect(page).to have_link('Klein, Rempel and Jones')
+      expect(page).to have_link('Wisozk, Hoeger and Bosco')
+
+      click_link('Schroeder-Jerde')
+
+      expect(current_path).to eq('/merchants/1')
+    end
+  end
+end
---
+++ spec/features/merchants/show_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Merchants Show View' do
+  describe 'User Story 1' do
+    # As a visitor,
+    # When I visit '/merchants'
+    # I should see a list of merchants by name
+    # and when I click the merchant's name
+
+    # I should be on page '/merchants/:id'
+    # And I should see a list of items that merchant sells.
+    it 'has has a list of each item belonging to that merchant' do
+      visit '/merchants/1'
+
+      expect(page).to have_content('Item Nemo Facere')
+      expect(page).to have_content('Item Rerum Est')
+      expect(page).to_not have_content('Item Adipisci Sint')
+    end
+  end
+end
